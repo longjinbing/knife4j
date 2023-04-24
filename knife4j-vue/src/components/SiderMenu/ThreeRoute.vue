4f2e671f0a4c21ab9a00fbf718461a1bac5f75d9
longjinbin <1106335838@qq.com>
2023-04-24 12:29:31 +0800

    优化

--- knife4j-vue/src/components/SiderMenu/ThreeRoute.vue
+++ knife4j-vue/src/components/SiderMenu/ThreeRoute.vue
@@ -8,7 +8,10 @@
   >
     <my-icon v-if="item.icon" :type="item.icon"></my-icon>
     <a-badge v-if="enableVersion&&item.hasNew" status="processing" title="新接口" style="margin-bottom:3px;" />
-    <span v-if="item.method" class="knife4j-menu-line">{{item.method}}</span>
+    <a-tag v-if="item.method === 'GET'" color="green">{{item.method}}</a-tag>
+    <a-tag v-if="item.method === 'POST' " color="blue">{{item.method}}</a-tag>
+    <a-tag v-if="item.method === 'PUT' " color="cyan">{{item.method}}</a-tag>
+    <a-tag v-if="item.method === 'DELETE' " color="red">{{item.method}}</a-tag>
     <span>{{item.name}}</span>
   </router-link>
 </template>
